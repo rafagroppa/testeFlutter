73f92743ff6c685e5d573e9406e0f3949e9b6c24
Rafael Groppa <rafagroppa@gmail.com>
2019-10-08 19:01:36 -0300

    Alteracoes

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var _check  = false;
+  // var _check  = false;
 
   void _incrementCounter() {
     setState(() { 
@@ -41,35 +41,55 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(),
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.camera), 
-            onPressed: () {
-              print("Click");
-            },),
-            Checkbox(
-              value: _check, 
-              onChanged: (bool value) {
-                setState(() {
-                  _check = value;
-                });
-              },)
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: Icon(Icons.camera), 
+      //       onPressed: () {
+      //         print("Click");
+      //       },),
+      //       Checkbox(
+      //         value: _check, 
+      //         onChanged: (bool value) {
+      //           setState(() {
+      //             _check = value;
+      //           });
+      //         },)
+      //   ],
+      // ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            //image: NetworkImage("https://cdn6.f-cdn.com/contestentries/68791/6145242/5336caec3b1ad_thumb900.jpg"),
+            image: AssetImage("assets/img/back.jpg"),
+            fit: BoxFit.cover
+          )           
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(30),
+                child: Image(
+                  image: AssetImage("assets/img/tinder.png"),
+                ),
+              ),
+              Text(
+                'GDG Tinder',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
